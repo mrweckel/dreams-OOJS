738db189e09a7d4a85fc9a0b2531b93e4dca1390
James Hwang & Matt <pair+jhwang90.mrweckel@devbootcamp.com>
2014-11-26 15:31:37 -0500

    require gems for oauth, set up initial google client

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,11 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+#Required for google oauth
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'json'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -26,5 +31,16 @@ module Dreams
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    $credentials = Google::APIClient::ClientSecrets.load
+
+    $authorization = Signet::OAuth2::Client.new(
+        :authorization_uri => $credentials.authorization_uri,
+        :token_credential_uri => $credentials.token_credential_uri,
+        :client_id => $credentials.client_id,
+        :client_secret => $credentials.client_secret,
+        :redirect_uri => $credentials.redirect_uris.first,
+        :scope => ['https://www.googleapis.com/auth/plus.me'])
+
+    $client = Google::APIClient.new
   end
 end
