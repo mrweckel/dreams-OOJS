1c92a143043995b9bf1e7e54a4b5a134f20f8857
mrweckel <mrweckel@gmail.com>
2014-11-28 13:47:53 -0500

    add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'dreams#index'
 
+    get "/login" => "sessions#new"
+    post "/login" => "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
