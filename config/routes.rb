6e1d6c04efe375a9a84951302d292a6c8f17499d
mrweckel <mrweckel@gmail.com>
2014-11-28 16:39:36 -0500

    remove session routes from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'dreams#index'
 
-    get "/login" => "sessions#new"
-    post "/login" => "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
