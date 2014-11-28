f91668e6b4b9f37ed51b7470cc60137b154cf8d4
jhwang90 <jhwang90@gmail.com>
2014-11-28 16:25:01 -0500

    Adds Google_OAuth test to verify that the scope is youtube

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
 group :test, :development do
+	gem 'jasmine'
   gem 'jasmine-rails'
   gem 'phantomjs'
 end
--- spec/javascripts/google_oauth_spec.js
+++ spec/javascripts/google_oauth_spec.js
@@ -1,5 +1,16 @@
 describe('Google_Oauth', function() {
+  
+	beforeEach(function() {
+		google = GoogleAuth;
+	});
+
   it("shoud be true", function() {
     expect(1 + 1).toBe(2);
   });
+
+  it("should have a scope of youtube", function() {
+  	console.log(google.Keys.scopes);
+  	expect(google.Keys.scopes == 'https://www.googleapis.com/auth/youtube').toBe(true);
+  });
+
 });
