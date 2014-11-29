342db6c0a82163db776340a47ef3dbb87cfb488d
unknownbreaker <rob.yang@gmail.com>
2014-11-28 22:25:17 -0500

    create new object for youtube view for list of titles

--- app/assets/javascripts/public/youtube_data.js
+++ app/assets/javascripts/public/youtube_data.js
@@ -70,42 +70,58 @@ YouTubeData.Account = {
         GoogleAuth.View.displayMessage(response.error.message);
       } else {
         // Get the jQuery wrapper for #video-list once outside the loop.
-        var videoList = $('#video-list');
-        $.each(response.items, function() {
-          // Exclude videos that don't have any views, since those videos
-          // will not have any interesting viewcount analytics data.
-          if (this.statistics.viewCount == 0) {
-            return;
-          }
 
-          var title = this.snippet.title;
-          var videoId = this.id;
+        console.log(response.items);
 
-          // Create a new <li> element that contains an <a> element.
-          // Set the <a> element's text content to the video's title, and
-          // add a click handler that will display Analytics data when invoked.
-          var liElement = $('<li>');
-          var aElement = $('<a>');
-          // The dummy href value of '#' ensures that the browser renders the
-          // <a> element as a clickable link.
-          aElement.attr('href', '#');
-          aElement.text(title);
-          aElement.click(function() {
-            console.log(videoId)
-            //  this is the part that needs to put in the array for
-          });
 
-          // Call the jQuery.append() method to add the new <a> element to
-          // the <li> element, and the <li> element to the parent
-          // list, which is identified by the 'videoList' variable.
-          liElement.append(aElement);
-          videoList.append(liElement);
-        });
 
-        if (videoList.children().length == 0) {
-          GoogleAuth.View.displayMessage('Your channel does not have any videos that have been viewed.');
-        }
+        YouTubeData.View.showVideoTitles(response);
+
       }
     });
+  }
+}
+
+
+
+YouTubeData.View = {
+
+  showVideoTitles: function(response) {
+    var videoList = $('#video-list');
+
+    $.each(response.items, function() {
+      // Exclude videos that don't have any views, since those videos
+      // will not have any interesting viewcount analytics data.
+      if (this.statistics.viewCount == 0) {
+        return;
+      }
+
+      var title = this.snippet.title;
+      var videoId = this.id;
+
+      // Create a new <li> element that contains an <a> element.
+      // Set the <a> element's text content to the video's title, and
+      // add a click handler that will display Analytics data when invoked.
+      var liElement = $('<li>');
+      var aElement = $('<a>');
+      // The dummy href value of '#' ensures that the browser renders the
+      // <a> element as a clickable link.
+      aElement.attr('href', '#');
+      aElement.text(title);
+      aElement.click(function() {
+        console.log(videoId)
+        //  this is the part that needs to put in the array for
+      });
+
+      // Call the jQuery.append() method to add the new <a> element to
+      // the <li> element, and the <li> element to the parent
+      // list, which is identified by the 'videoList' variable.
+      liElement.append(aElement);
+      videoList.append(liElement);
+    });
+
+    if (videoList.children().length == 0) {
+      GoogleAuth.View.displayMessage('Your channel does not have any videos that have been viewed.');
+    }
   }
 }
