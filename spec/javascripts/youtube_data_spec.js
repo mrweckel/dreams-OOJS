c1d3809f429a92e6938c732548ee1f5702b97619
jhwang90 <jhwang90@gmail.com>
2014-11-29 00:45:02 -0500

    Adds jasmine test to test getUserChannel calls the request method

--- spec/javascripts/youtube_data_spec.js
+++ spec/javascripts/youtube_data_spec.js
@@ -7,10 +7,6 @@ describe('Youtube_Data', function() {
   afterEach (function() { youtube_account = undefined; 
     youtube = undefined;
   });
-	// beforeEach(function() {
-	// 	youtube = YouTubeData;
-	// 	youtube_account = youtube.Account
-	// });
 
 // Spy allows you to fake a method instead of actually firing/running the method
   it("should spy on getUserChannel method", function() {
@@ -31,10 +27,10 @@ describe('Youtube_Data', function() {
   	expect(spy).toHaveBeenCalled();
   });
 
-  // it("should have getUserChannel call request execute method", function() {
-  //   var spy = spyOn(youtube_account, 'getUserChannel');
-  //   youtube_account.getUserChannel();
-  //   expect(spy).toHaveBeenCalled();
-  // });
+  it("should have getUserChannel call request execute method", function() {
+    var spy = spyOn(youtube_account, 'getUserChannel');
+    youtube_account.getUserChannel();
+    expect(youtube_account.getUserChannel.request).not.toBe(null);
+  });
 
 });
