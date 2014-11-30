062e52fa0243cf4cd6534412f6d173c313296220
Michael Angelo <yamikamisama@gmail.com>
2014-11-30 14:10:14 -0500

    timing for fade in

--- app/assets/javascripts/public/youtube_data.js
+++ app/assets/javascripts/public/youtube_data.js
@@ -105,24 +105,24 @@ YouTubeData.Account = {
 
 
         VideoPlayer.main(videoArr);
-        setTimeout(function() { $('#player').fadeOut(2000)
-                                $('#player').fadeIn(4000)
-                              }, 10000);
-        setTimeout(function() { $('#player').fadeOut(2000)
-                                $('#player').fadeIn(4000)
-                              }, 22000);
-        setTimeout(function() { $('#player').fadeOut(2000)
-                                $('#player').fadeIn(4000)
-                              }, 34000);
-        setTimeout(function() { $('#player').fadeOut(5000)
-                                $('#player').fadeIn(5000)
-                              }, 40000);
-        setTimeout(function() { $('#player').fadeOut(5000)
-                                $('#player').fadeIn(5000)
-                              }, 50000);
-        setTimeout(function() { $('#player').fadeOut(5000)
-                                $('#player').fadeIn(5000)
-                              }, 60000);
+        // setTimeout(function() { $('#player').fadeOut(2000)
+        //                         $('#player').fadeIn(4000)
+        //                       }, 10000);
+        // setTimeout(function() { $('#player').fadeOut(2000)
+        //                         $('#player').fadeIn(4000)
+        //                       }, 22000);
+        // setTimeout(function() { $('#player').fadeOut(2000)
+        //                         $('#player').fadeIn(4000)
+        //                       }, 34000);
+        // setTimeout(function() { $('#player').fadeOut(5000)
+        //                         $('#player').fadeIn(5000)
+        //                       }, 40000);
+        // setTimeout(function() { $('#player').fadeOut(5000)
+        //                         $('#player').fadeIn(5000)
+        //                       }, 50000);
+        // setTimeout(function() { $('#player').fadeOut(5000)
+        //                         $('#player').fadeIn(5000)
+        //                       }, 60000);
       }
     });
   }
