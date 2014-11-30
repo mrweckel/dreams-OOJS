a6057c3fd5d0c45e21fc26d6377387dc1970faa8
James Hwang <jhwang90@gmail.com>
2014-11-30 18:01:42 -0500

    Removes console.logs to minimize debugging noise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
+before_filter :require_user, only: :show
+
+  def new
+    @user = User.new()
+  end
+
   def create
+<<<<<<< HEAD
 <<<<<<< HEAD
     if !User.exists?(user_id: params[:userId])
       @user = User.new(user_id: params[:userId])
@@ -31,7 +38,38 @@ class UsersController < ApplicationController
 =======
     @user = User.create(user_params)
     redirect_to
+=======
+    respond_to do |format|
+      format.html
+      format.json
+    end
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id # this will need to be YT_uid
+      else
+        # redirect_to #unauthorized_user_path
+      end
   end
+
+  def show
+    @user = User.find(sessions[:id])
+  end
+
+# Used for getting youtube API response specific to google-authorized user
+  def fileContent
+    @user = User.where(:user_response)
+  end
+
+  def require_user
+    # redirect_to #unauthorized_user_path unless current_user
+>>>>>>> Removes console.logs to minimize debugging noise
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:YT_uid]) if session[:YT_uid]
+    rescue ActiveRecord::RecordNotFound
+  end
+
 
   # Use this to privatize user's google/youtube id
   # def user_params
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,12 @@
 class VideosController < ApplicationController
+
+  def new
+    @video = Video.new(video_params)
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @video.save
+  end
+
 end
