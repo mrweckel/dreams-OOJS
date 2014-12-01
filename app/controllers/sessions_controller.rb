6df8b3af2aeaa3b6d5850ecfa096b1ba2625b2ba
James Hwang <jhwang90@gmail.com>
2014-12-01 18:12:19 -0500

    Sets up sessions for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(username: params[:YT_uid])#.try(:authenticate, params[:password])#youtube api authentication
+    p "in sessions create"
+    @user = User.find_by(username: params[:user_id])
     if @user
-      session[:YT_uid] = @user.id #this will be @user.YT_uid from the API call
+      p "in sessions create user exists"
+      session[:user_id] = @user.user_id #this will be @user.YT_uid from the API call
       redirect_to root_path #path with random+personalized choices
     else
+      p "in sessions create user doesnt exist"
       redirect_to root_path #path with only keyword searchbar
     end
   end
 
   def destroy
-    session[:YT_uid] = nil
+    session[:user_id] = nil
     redirect_to root_path
   end
 
