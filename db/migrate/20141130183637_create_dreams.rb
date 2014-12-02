00ca809b1e49513f4b6808965fc19384683c3169
James Hwang <jhwang90@gmail.com>
2014-12-02 02:40:30 -0500

    Removes videos migration

--- db/migrate/20141130183637_create_dreams.rb
+++ db/migrate/20141130183637_create_dreams.rb
@@ -3,6 +3,10 @@ class CreateDreams < ActiveRecord::Migration
     create_table :dreams do |t|
       t.references :user
       t.string :dream_name
+      t.string :YT_video_id
+      t.string :duration
+      t.string :start_time
+      t.string :end_time
 
       t.timestamps
     end
