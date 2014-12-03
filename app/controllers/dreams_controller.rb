b73da623a19dd1b9cc77a93a3ee55b616720d747
James Hwang & Steph Reaves <pair+jhwang90.stephreaves@devbootcamp.com>
2014-12-02 20:54:46 -0500

    Fixes Dream#new

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -6,17 +6,17 @@ class DreamsController < ApplicationController
   end
 
   def new
-    # steven sez: i don't think you need dream_params here do you?
-    @dream = Dream.new(dream_params)
+    @dream = Dream.new
   end
 
   def create
     @user = User.find_by(user_id: session[:user_id])
     @dream = @user.dreams.create(dream_params)
 
-    # respond_to do |format|
-    #   format.json { render json: @dream }
-    # end
+    respond_to do |format|
+      format.html
+      format.json { render json: @dream }
+    end
   end
 
   def show
