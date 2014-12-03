7abf94379e1299ab458b657ba9a91c5602667069
James Hwang & Steph Reaves <pair+jhwang90.stephreaves@devbootcamp.com>
2014-12-02 21:54:04 -0500

    Successfully passes JSON objects back and forth to/from Rails database & JS YouTube functions

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -18,22 +18,15 @@ class DreamsController < ApplicationController
     @user = User.find_by(user_id: session[:user_id])
     properties = params.to_json
     @dream = @user.dreams.create(properties: properties)
-    p "*"*100
-    params.each do |param|
-      param.each do |video|
-        # video.each do |video_attr|
-        #   p "*"*200
-        #     p video_attr
-        #   p "*"*200
-        # end
-      end
-    end
-    p "*"*100
 
   end
 
   def show
-    @dreams = Dream.find(sessions[:user_id])
+    @dream = Dream.find(1)
+
+    respond_to do |format|
+      format.json { render json: @dream.properties }
+    end
   end
 
   private
