bcad06185d0021b5697ea2c932ec8fb9abe72d82
Steph Reaves <stephanie.reaves@gmail.com>
2014-12-10 22:20:49 -0500

    removed a if & else statement in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
 
   def create
-    if !User.exists?(user_id: params[:userId])
-      @user = User.new(user_id: params[:userId])
-      if @user.save
-        session[:user_id] = @user.user_id
-      else
-        redirect_to(root_path)
-      end
-    else
-      @user = User.where(user_id: params[:userId])
-      session[:user_id] = @user.user_id
-    end
-  end
-
-  def index
     p "*"*200
     p params[:userId]
     p "*"*200
-    p session[:user_id]
-    user = User.where(user_id: session[:user_id])
-    p "*"*200
-    p user
-    p "*"*200
-    @dreams = user.dreams
-    p @dreams
+    if !User.exists?(user_id: params[:userId])
+        p "*"*200
+        p @user = User.create(user_id: params[:userId])
+        p "*"*200
+        p session[:user_id] = @user.id
+    else
+        redirect_to(root_path)
+    end
   end
+
+  # def index
+  #   p "*"*200
+  #   p params[:userId]
+  #   p "*"*200
+  #   p session[:user_id]
+  #   user = User.where(user_id: session[:user_id])
+  #   p "*"*200
+  #   p user
+  #   p "*"*200
+  #   @dreams = user.dreams
+  #   p @dreams
+  # end
 
   def logout
     session[:user_id] = nil
