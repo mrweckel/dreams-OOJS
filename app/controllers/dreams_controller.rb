312ac0d37e8e0631ff183952e68ff6d7772bf2ed
jhwang90 <jhwang90@gmail.com>
2014-12-22 16:28:51 -0800

    Add users#destroy to give discard-ability

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -9,18 +9,24 @@ class DreamsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(user_id: session[:user_id])
+    user = User.find_by(user_id: session[:user_id])
     video_properties = params.to_json
-    @dream = @user.dreams.create(video_properties: video_properties)
+    dream = user.dreams.create(video_properties: video_properties)
     #is saving the dream with the params passed in. need to test with click implemented
+  end
+
+# If discard was clicked at the end of the dream, it will destroy that dream sequence
+  def destroy
+    user = User.find_by(user_id: session[:user_id])
+    user.dreams.last.destroy
   end
 
 # Fix this method to be able to find the dream by the dream_id provided/clicked by user
   def show
-    @dream = Dream.find()
+    dream = Dream.find()
 
     respond_to do |format|
-      format.json { render json: @dream.video_properties }
+      format.json { render json: dream.video_properties }
     end
   end
 
